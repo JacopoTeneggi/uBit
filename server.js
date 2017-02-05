fe8da242dbd72b992b81652c3235e651bf464e69
Jacopo Teneggi <jacopoteneggi@live.com>
2017-02-05 16:42:49 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const ws = require('ws');
+const noble = require('noble');
+
+const wss = new WebSocket.Server({
+    perMessageDeflate: false,
+    port: 8080
+});
+console.log(`Server listening on port ${8080}`);
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(`Received ${message}`);
+    });
+
+    ws.send('something');
+});
